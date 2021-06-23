41e4a81521e87412efb77d5e15d6e18df9dac677
jaimegsandoval <jaime.devs@gmail.com>
2021-06-22 17:50:40 -0700

    Completed practice exercise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState('');
+  // set to null by default because the todoEditing is going to be the id of the todo that we're editing. To start out we won't be editing anything so we set the initial value in useState to null
+  const [todoEditing, setTodoEditing] = useState(null);
+  const [editingText, setEditingText] = useState('');
+
+
+  useEffect(() => {
+    const temp = sessionStorage.getItem('todos');
+    const loadedTodos = JSON.parse(temp);
+    if(loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, [])
+
+  useEffect(() => {
+    const temp = JSON.stringify(todos);
+    sessionStorage.setItem('todos', temp);
+  }, [todos])
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,8 +51,23 @@ function App() {
     setTodos(updatedTodos);
   };
 
+  const editTodo = (id) => {
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.text = editingText
+      }
+      // return returns every todo, but if the todo.id is equal ti the id passed into editTodo it'll have the new edited text
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+    setTodoEditing(null); // set the id back to null for our editing logic
+    setEditingText('');
+  }
+
   return (
     <div className="form-container">
+
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -44,10 +76,14 @@ function App() {
         />
         <button type="submit">Add Todo</button>
       </form>
+
       {todos.map((todo, i) => {
         return (
           <>
-            <h1 key={todo.id}>{todo.text}</h1>
+          {/* todoEditing is an id of a todo */}
+          {todoEditing === todo.id ? (<input type="text" onChange={(e) => setEditingText(e.target.value)} value={editingText} />) : (<h1 key={todo.id}>{todo.text}</h1>)}
+
+
             <button key={i} onClick={() => deleteTodo(todo.id)}>
               Delete Todo
             </button>
@@ -56,6 +92,11 @@ function App() {
               onChange={() => toggleComplete(todo.id)}
               checked={todo.completed}
             />
+
+            {todoEditing === todo.id ? (<button onClick={() => editTodo(todo.id)}>Submit Edit</button>) : (<button onClick={() => setTodoEditing(todo.id)}>Edit Todo</button>)}
+
+
+
           </>
         );
       })}
