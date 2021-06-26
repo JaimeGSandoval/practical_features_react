38587d75c2c895f11cf0789b6d7adba956b9256a
jaimegsandoval <jaime.devs@gmail.com>
2021-06-26 14:52:56 -0700

    Added simple layout and added useReducer for state

---
+++ expense-tracker/src/context/AppReducer.js
@@ -0,0 +1,8 @@
+// a reducer hook basically specifies the application's state changes in response to certain actions to our context.
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
---
+++ expense-tracker/src/context/GlobalState.js
@@ -0,0 +1,32 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+// So we import AppReducer and pass it to useReducer which is in the provider, and we can access state values from our initial state and we're passing that into the value that's in the provider.
+
+// initial state
+const initialState = {
+  transactions: [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 },
+  ],
+};
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+// create provider
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
