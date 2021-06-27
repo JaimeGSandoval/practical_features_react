ecd0e5257a048da4b85a4f9b6c4294d4f094266c
jaimegsandoval <jaime.devs@gmail.com>
2021-06-27 16:02:29 -0700

    Set state for characters and added api call for all characters w/ useEffeect

--- breaking-bad-api/src/App.js
+++ breaking-bad-api/src/App.js
@@ -1,7 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { Header } from './components/ui/Header';
+import axios from 'axios';
 import './App.scss';
 
 function App() {
-  return <div className="container">BANKAI</div>;
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const result = await axios.get(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+      setCharacters(result.data);
+      setIsLoading(false);
+    };
+
+    fetchCharacters();
+  }, []);
+
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
