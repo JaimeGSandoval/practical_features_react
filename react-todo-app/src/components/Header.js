a337933e67d636d85a29346d9c0960895179c0d1
jaimegsandoval <jaime.devs@gmail.com>
2021-06-29 20:38:45 -0700

    Added local storage for todos

--- react-todo-app/src/components/Header.js
+++ react-todo-app/src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
           fontWeight: '600',
           marginBottom: '2rem',
           lineHeight: '1em',
-          color: '#ececec',
+          color: '#333',
           textTransform: 'lowercase',
           textAlign: 'center',
         }}
