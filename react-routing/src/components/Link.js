5e61f371e1e8361a4c5dc7b795b4c938ceccc3bf
jaimegsandoval <jaime.devs@gmail.com>
2021-07-03 19:12:02 -0700

    Completed the routing exercises

---
+++ react-routing/src/components/Link.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Link = ({ href, className, children }) => {
+  const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
+    event.preventDefault();
+    window.history.pushState({}, '', href);
+
+    // These two lines of code will communicate to the route components that the route has just changed.
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+  };
+
+  return (
+    <a onClick={onClick} href={href} className={className}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
--- react-routing/src/components/Route.js
+++ react-routing/src/components/Route.js
@@ -1,5 +1,25 @@
+import { useState, useEffect } from 'react';
+
+// gotta set up a route handler to listen for that navigation event. Anytime we set up an event listener inside of a component it's usually a sign that we need to define a useState hook.
+
+// Every time onLocationChange gets called we have to tell the Route component to update itself and re-render itself. When it re-renders itself we're going to fetch whatever the current window.location.pathname is and use that to decide whether or not this route component should now display its child or hide it.
+
+// We're going to create a piece of state that follows window.location.pathname and its value. The window.location.pathname is always kept up to date. It'll always reflect whatever the current pathname is inside the address bar. The state will have the sole purpose of trying to get the route component to re-render.
+
 const Route = ({ path, children }) => {
-  return window.location.pathname === path ? children : null;
+  // the only reason this piece of state exists is just to get our route to update.
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = (event) => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', onLocationChange);
+    return () => window.removeEventListener('popstate', onLocationChange);
+  }, []);
+
+  return currentPath === path ? children : null;
 };
 
 export default Route;
