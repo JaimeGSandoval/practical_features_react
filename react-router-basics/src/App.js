e68f1578c6d1746ca460bcb2c9abc3d043983131
jaimegsandoval <jaime.devs@gmail.com>
2021-07-06 00:04:56 -0700

    Added reacter router

---
+++ react-router-basics/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import { useHistory } from 'react-router-dom';
+
+export default function App() {
+  const history = useHistory();
+  return (
+    <div className="App">
+      <button>
+        <Link to="/about"> About Us </Link>
+      </button>
+      <button onClick={() => history.push('/contact')}> Contact Us </button>
+      <button onClick={() => history.push('/')}> Home Page </button>
+      <Switch>
+        <Route exact path="/about" render={(props) => <About {...props} />} />
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
