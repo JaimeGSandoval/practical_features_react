c4e5278a156550e45d4f4bfab05341da89d530d9
jaimegsandoval <jaime.devs@gmail.com>
2021-07-08 14:34:28 -0700

    Added a withRouter to the NavBar for history access

--- router-patterns-steele/src/Navbar.js
+++ router-patterns-steele/src/Navbar.js
@@ -1,21 +1,28 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import "./Navbar.css";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import './Navbar.css';
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
     this.handleLogin = this.handleLogin.bind(this);
+    // this.handleBack = this.handleBack.bind(this);
   }
   handleLogin() {
-    alert("LOGGED YOU IN!");
-    this.props.history.push("/food/salmon");
+    alert('LOGGED YOU IN!');
+    this.props.history.push('/food/salmon');
   }
+
+  // you can create a method to fire onClick like here or you can simply use props.history.goBack inline
+  // handleBack() {
+  //   this.props.history.goBack();
+  // }
 
   render() {
     return (
-      <div className='Navbar'>
+      <div className="Navbar">
         <button onClick={this.handleLogin}>Log In</button>
+        {/* <button onClick={this.handleBack}>go back</button> */}
         <button onClick={this.props.history.goBack}>go back</button>
       </div>
     );
