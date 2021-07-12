cfb691d433d4cbe5b2686a793089e204b0831645
jaimegsandoval <jaime.devs@gmail.com>
2021-07-12 00:10:35 -0700

    small refactor practice

--- videos-hooks/src/components/SearchBar.js
+++ videos-hooks/src/components/SearchBar.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 
+// app.js sends a function as a prop to the search component
+// search component has state (term) for the input field, controlled.
+// the function sent to the search component as a prop from app.js is placed in the onSubmit function
+// when the form is submitted, te onSubmit function gets called, which calls the function sent to the search component by app.js. The function gets the state term as an argument. App.js uses the term to make a get request.
+// the request comes back with videos based on the term.
+
 const SearchBar = ({ onFormSubmit }) => {
   const [term, setTerm] = useState('');
 
